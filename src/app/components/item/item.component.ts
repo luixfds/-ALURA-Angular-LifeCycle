dc69acf7b1723d8d08785fccc231f59ff4003437
luixfds <l.fds@yahoo.com>
2024-01-04 21:04:51 -0300

    projeto final

---
+++ src/app/components/item/item.component.ts
@@ -0,0 +1,42 @@
+import { outputAst } from '@angular/compiler';
+import { Component, EventEmitter, Input, OnChanges, OnDestroy, OnInit, Output } from '@angular/core';
+import { faPen, faTrash } from '@fortawesome/free-solid-svg-icons';
+import { Item } from 'src/app/interfaces/iItem';
+
+@Component({
+  selector: 'app-item',
+  templateUrl: './item.component.html',
+  styleUrls: ['./item.component.css']
+})
+export class ItemComponent implements OnInit, OnChanges, OnDestroy {
+  @Input() item!: Item;
+  @Output() emitindoItemParaEditar = new EventEmitter();
+  @Output() emitindoIdParaDeletar = new EventEmitter();
+
+  faPen = faPen;
+  faTrash = faTrash
+
+  constructor() { }
+
+  ngOnInit(): void {}
+
+  ngOnChanges(){}
+
+  ngOnDestroy(): void {}
+
+  editarItem(){
+    this.emitindoItemParaEditar.emit(this.item);
+  }
+
+  deletarItem(){
+    this.emitindoIdParaDeletar.emit(this.item.id);
+  }
+
+  checarItem() {
+    if(this.item.comprado == true){
+      this.item.comprado = false;
+    }else{
+      this.item.comprado = true;
+    }
+  }
+}
